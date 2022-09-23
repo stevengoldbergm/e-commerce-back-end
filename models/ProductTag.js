99d308b534c852b37a48a91368ebfb5da3234c88
SG_Demi <stevengoldbergm@gmail.com>
2022-09-22 23:52:39 -0500

    Updating Model Notes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,10 +1,14 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection');
 const Product = require('./Product');
 
+// Initialize ProductTag model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
 
+// set up fields and rules for ProductTag model
 ProductTag.init(
   {
     id: {
